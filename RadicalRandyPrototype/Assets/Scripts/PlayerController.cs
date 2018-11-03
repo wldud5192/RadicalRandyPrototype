1556fa17c4fca8fa88e631c027cdf7668bf9751e
Jiyoung Kim <1010215@student.sae.edu.au>
2018-11-04 00:59:19 +1000

    Initial commit

---
+++ RadicalRandyPrototype/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	public float speed;
+
+
+	void FixedUpdate ()
+	{
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+		if (movement.magnitude > 0.0001f) {
+			transform.rotation = Quaternion.Slerp (transform.rotation, Quaternion.LookRotation (movement), 0.15F);
+		}
+		transform.Translate (movement * -speed * Time.deltaTime, Space.World);
+	}
+}
---
+++ RadicalRandyPrototype/Assets/Scripts/animationScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class animationScript : MonoBehaviour {
+
+	Animator animator;
+
+	// Use this for initialization
+	void Start () {
+
+		animator = gameObject.GetComponent<Animator> ();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.anyKey) {
+			animator.SetBool ("isWalking", true);
+		} else {
+			animator.SetBool ("isWalking", false);
+		}
+	}
+}
