f34e31ec2211b9a3af42c031c84aa7c2c9b6da19
Jiyoung Kim <1010215@student.sae.edu.au>
2018-11-04 22:59:42 +1000

    -
    -

--- RadicalRandyPrototype/Assets/Ping.cs
+++ RadicalRandyPrototype/Assets/Ping.cs
@@ -4,16 +4,47 @@ using UnityEngine;
 
 public class Ping : MonoBehaviour
 {
+    public bool scanner;
     public GameObject ping;
 
 
+    void Start()
+    {
+
+    }
+
     void OnTriggerStay(Collider col)
     {
-        if (col.gameObject.tag == "Player")
+        if (col.gameObject.tag == "Player" && this.gameObject != null)
         {
-            ping.GetComponent<MeshRenderer> ().enabled = true;
+            scanner = true;
+            ping.GetComponent<MeshRenderer>().enabled = true;
+            StartCoroutine(Scale(10));
 
-            
+        } 
+    }
+    IEnumerator Scale(float time)
+    {
+
+        Vector3 originalScale = ping.transform.localScale;
+        Vector3 destinationScale = new Vector3(500.0f, 500.0f);
+
+        
+        {
+            float currentTime = 0.0f;
+
+            do
+            {
+
+                ping.transform.localScale = Vector3.Lerp(originalScale, destinationScale, currentTime / time);
+                currentTime += Time.deltaTime;
+                yield return null;
+            } while (currentTime <= time);
         }
+
+        Destroy(this.gameObject);
+        
+
     }
 }
+
