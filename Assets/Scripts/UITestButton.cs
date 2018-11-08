1cd7191f7240d51357893d9e2a9956d46eaf8869
Jiyoung Kim <wldud5192@naver.com>
2018-11-08 13:26:49 +1000

    HUD+UI

---
+++ Assets/Scripts/UITestButton.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UITestButton : MonoBehaviour {
+
+    public GameObject[] objArray;
+    private int activateNext = 0;
+
+    void Start()
+    {
+        objArray = GameObject.FindGameObjectsWithTag("UI");
+        foreach (GameObject i in objArray)
+        {
+            i.SetActive(false);
+        }
+    }
+
+
+    public void EnableObject ()
+    {
+        Debug.Log(activateNext);
+        if (activateNext <= objArray.Length)
+        {
+               objArray[activateNext].SetActive(true);
+                activateNext++;
+        } else 
+        {
+            activateNext = 0;
+            foreach (GameObject i in objArray)
+            {
+                i.SetActive(false);
+            }
+
+        }
+    }
+}
