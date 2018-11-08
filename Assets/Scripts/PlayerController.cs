5aa8af8045295d7e531511f681824b1a2641ce33
SAE\jcriss2 <1008679@student.sae.edu.au>
2018-11-08 13:44:53 +1000

    Adding Door Management To create a new Door New GameObject and tag it with the respective door colour. New GameObject and tag it with the respective key colour. change the key's collider to be a trigger.
    Possible issues
    BasePos.y = -6f; may need to be changed due to scale

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,8 +4,9 @@ using System.Collections;
 public class PlayerController : MonoBehaviour {
 
 	public float speed;
+    GameObject[] Unlocked;
 
-	void FixedUpdate ()
+    void FixedUpdate ()
 	{
 		float moveHorizontal = Input.GetAxis ("Horizontal");
 		float moveVertical = Input.GetAxis ("Vertical");
@@ -16,4 +17,49 @@ public class PlayerController : MonoBehaviour {
 		}
 		transform.Translate (movement * speed * Time.deltaTime, Space.World);
 	}
+
+
+    // Unlocking Doors
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.tag == ("RedKey"))                         //Identify the current key you are inside of
+        {
+            Unlocked = GameObject.FindGameObjectsWithTag("RedDoor");    //Puts all Doors of that colour into an array
+            foreach (GameObject Unlocked in Unlocked)                 
+            {
+                Vector3 BasePos = Unlocked.transform.position;          // BasePos is given the current position of each door
+                BasePos.y = -6f;                                        // BasePos is then lowered to be (presumably) below field
+                if (Unlocked.transform.position.y > BasePos.y)          // if the current position is above the basepos then the door is lowered
+                {
+                    Unlocked.transform.position = Vector3.MoveTowards(Unlocked.transform.position, BasePos, 0.5f);
+                }
+            }
+        }
+        else if (other.gameObject.tag == ("BlueKey"))                   //Same as applies above
+        {
+            Unlocked = GameObject.FindGameObjectsWithTag("BlueDoor");
+            foreach (GameObject Unlocked in Unlocked)
+            {
+                Vector3 BasePos = Unlocked.transform.position;
+                BasePos.y = -6f;
+                if (Unlocked.transform.position.y > BasePos.y)
+                {
+                    Unlocked.transform.position = Vector3.MoveTowards(Unlocked.transform.position, BasePos, 0.5f);
+                }
+            }
+        }
+        else if (other.gameObject.tag == ("GreenKey"))                     //Same as applies above
+        {
+            Unlocked = GameObject.FindGameObjectsWithTag("GreenDoor");
+            foreach (GameObject Unlocked in Unlocked)
+            {
+                Vector3 BasePos = Unlocked.transform.position;
+                BasePos.y = -6f;
+                if (Unlocked.transform.position.y > BasePos.y)
+                {
+                    Unlocked.transform.position = Vector3.MoveTowards(Unlocked.transform.position, BasePos, 0.5f);
+                }
+            }
+        }
+    }
 }
