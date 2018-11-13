e97b880f590c156147f4feef2eff6a6c4ced7b54
jiyoungkim1010215 <1010215@student.sae.edu.au>
2018-11-13 14:29:58 +1000

    1. General clean-up (Broken Prefabs are replaced with new) 2. Scene > FunctionalTestWithoutLevel.scene 3. Map FOV (Walls are visible until player departs) 4. Player dissolves within VPN

---
+++ Assets/Scripts/LightSettings.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightSettings : MonoBehaviour
+{
+
+
+    public bool playerIsAtSpawn;
+
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            playerIsAtSpawn = true;
+        }
+
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            playerIsAtSpawn = false;
+        }
+    }
+    void Update()
+    {
+        if (playerIsAtSpawn) { 
+        RenderSettings.ambientLight = Color.Lerp(Color.white, Color.black, Mathf.PingPong(Time.time, 1));
+    } else
+        {
+            RenderSettings.ambientLight = Color.black;
+        }
+
+    }
+}
