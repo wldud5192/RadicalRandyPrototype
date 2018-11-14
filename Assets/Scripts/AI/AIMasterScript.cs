3ce7da1f7c8cd53a0232785a16a823ec451e48c4
Jiyoung Kim <wldud5192@naver.com>
2018-11-15 09:29:32 +1000

    Quick update

--- Assets/Scripts/AI/AIMasterScript.cs
+++ Assets/Scripts/AI/AIMasterScript.cs
@@ -8,295 +8,293 @@ using UnityEngine.SceneManagement;
 [RequireComponent(typeof(NavMeshAgent))]
 public class AIMasterScript : MonoBehaviour
 {
-	public enum AIState
-	{
-		AI_Patroling,
-		AI_Chasing,
-		AI_Searching,
-		AI_Alerted
-	};
+    public enum AIState
+    {
+        AI_Patroling,
+        AI_Chasing,
+        AI_Searching,
+        AI_Alerted
+    };
 
-	public enum SearchType
-	{
-		Search_Snake,
-		Search_Horizontal,
-		Search_Vertical
-	}
+    public enum SearchType
+    {
+        Search_Snake,
+        Search_Horizontal,
+        Search_Vertical
+    }
 
-	float viewDistance;
-	float walkSpeed;
-	float runSpeed;
-	public GameObject[] patrolPositions;
-	public int nextPatrolPosition;
-	bool canRun;
-	bool isLethal;
-	bool isChasingPlayer;
-	NavMeshAgent navAgent;
-	Vector3 playersLastLocation;
-	float attackDistance;
-	float searchTime;
-	float searchDistance;
-	public float minDistanceFromCheckpoint;
+    float viewDistance;
+    float walkSpeed;
+    float runSpeed;
+    public GameObject[] patrolPositions;
+    public int nextPatrolPosition;
+    bool canRun;
+    bool isLethal;
+    bool isChasingPlayer;
+    NavMeshAgent navAgent;
+    Vector3 playersLastLocation;
+    float attackDistance;
+    float searchTime;
+    float searchDistance;
+    public float minDistanceFromCheckpoint;
 
-	public Vector3 alertPosition;
+    public Vector3 alertPosition;
 
-	public AI_SO aiLogic;
-	public AIState currentState;
-	public SearchType searchMethod;
+    public AI_SO aiLogic;
+    public AIState currentState;
+    public SearchType searchMethod;
 
-	public bool moveUpOrRight;
+    public bool moveUpOrRight;
 
-	GameObject player;
+    GameObject player;
 
-	//public float viewDistance = 15;
+    //public float viewDistance = 15;
 
-	void Start()
-	{
-		GetOrCreateNavMesh();
-		GetSOData();
+    void Start()
+    {
+        GetOrCreateNavMesh();
+        GetSOData();
 
-		moveUpOrRight = true;
-	}
+        moveUpOrRight = true;
+    }
 
-	void Update()
-	{
+    void Update()
+    {
 
-		switch (currentState)
-		{
-			case AIState.AI_Patroling:
-				Search();
+        switch (currentState)
+        {
+            case AIState.AI_Patroling:
+                Search();
 
-				//Patrol();
-				break;
+                //Patrol();
+                break;
 
-			case AIState.AI_Chasing:
-				//Chase();
-				Search();
+            case AIState.AI_Chasing:
+                //Chase();
+                Search();
 
-				break;
+                break;
 
-			case AIState.AI_Searching:
-				Search();
+            case AIState.AI_Searching:
+                Search();
 
-				break;
+                break;
 
-			case AIState.AI_Alerted:
-				Search();
+            case AIState.AI_Alerted:
+                Search();
 
-				//Alert(alertPosition);
-				break;
+                //Alert(alertPosition);
+                break;
 
-			default:
-				break;
-		}
-	}
+            default:
+                break;
+        }
+    }
 
-	void Patrol()
-	{
-		if (Vector3.Distance(transform.position, patrolPositions[nextPatrolPosition].transform.position) < minDistanceFromCheckpoint) //Todo - Replace with SO Editable
-		{
-			if (patrolPositions.Length - 1 < nextPatrolPosition + 1)
-			{
-				nextPatrolPosition = 0;
-			}
-			else
-			{
-				nextPatrolPosition += 1;
-			}
-		}
-		navAgent.SetDestination(patrolPositions[nextPatrolPosition].transform.position);
-	}
+    void Patrol()
+    {
+        if (Vector3.Distance(transform.position, patrolPositions[nextPatrolPosition].transform.position) < minDistanceFromCheckpoint) //Todo - Replace with SO Editable
+        {
+            if (patrolPositions.Length - 1 < nextPatrolPosition + 1)
+            {
+                nextPatrolPosition = 0;
+            }
+            else
+            {
+                nextPatrolPosition += 1;
+            }
+        }
+        navAgent.SetDestination(patrolPositions[nextPatrolPosition].transform.position);
+    }
 
-	void Chase()
-	{
-		isChasingPlayer = true;
+    void Chase()
+    {
+        isChasingPlayer = true;
 
-	}
+    }
 
-	void Search()
-	{
-		Vector3 randomNavPosition = new Vector3(Random.Range(-50, 50), Random.Range(-50, 50));
-		NavMeshHit hitPoint;
-		NavMesh.SamplePosition(randomNavPosition, out hitPoint, 50, 1);
+    void Search()
+    {
+        Vector3 randomNavPosition = new Vector3(Random.Range(-50, 50), Random.Range(-50, 50));
+        NavMeshHit hitPoint;
+        NavMesh.SamplePosition(randomNavPosition, out hitPoint, 50, 1);
 
-		//Scan Vector 3.Up * dist + -Vector3.forward * dist
+        //Scan Vector 3.Up * dist + -Vector3.forward * dist
 
-		float distance = 0.0f;
+        float distance = 0.0f;
 
-		if (searchMethod == SearchType.Search_Horizontal)
-		{
-			RaycastHit hitRight;
-			RaycastHit hitLeft;
+        if (searchMethod == SearchType.Search_Horizontal && player == null)
+        {
+            RaycastHit hitRight;
+            RaycastHit hitLeft;
 
-			if (moveUpOrRight)
-			{
-				if (Physics.Raycast(this.transform.position, Vector3.right, out hitRight, 500))
-				{
-					distance = Vector3.Distance(transform.position, hitRight.transform.position);
-					if (!ScanForPlayer(hitRight) && player == null)
-					{
-						navAgent.SetDestination(hitRight.transform.position);
-						Debug.Log(distance);
+            if (moveUpOrRight)
+            {
+                if (Physics.Raycast(this.transform.position, Vector3.right, out hitRight, 500))
+                {
+                    distance = Vector3.Distance(transform.position, hitRight.transform.position);
+                    if (!ScanForPlayer(hitRight) && player == null)
+                    {
+                        navAgent.SetDestination(hitRight.transform.position);
+                        Debug.Log(distance);
 
-						if (distance < 5)
-						{
-							moveUpOrRight = false;
-						}
-					}
-				}
-			}
-			else
-			{
-				if (Physics.Raycast(this.transform.position, -Vector3.right, out hitLeft, 500))
-				{
-					if (!ScanForPlayer(hitLeft) && player == null)
-					{
-						distance = Vector3.Distance(transform.position, hitLeft.transform.position);
+                        if (distance < 5)
+                        {
+                            moveUpOrRight = false;
+                        }
+                    }
+                }
+            }
+            else
+            {
+                if (Physics.Raycast(this.transform.position, -Vector3.right, out hitLeft, 500))
+                {
+                    if (!ScanForPlayer(hitLeft) && player == null)
+                    {
+                        distance = Vector3.Distance(transform.position, hitLeft.transform.position);
 
-						navAgent.SetDestination(hitLeft.transform.position);
+                        navAgent.SetDestination(hitLeft.transform.position);
 
-						if (distance < 5)
-						{
-							moveUpOrRight = true;
-						}
-					}
-				}
-			}
-		}
+                        if (distance < 5)
+                        {
+                            moveUpOrRight = true;
+                        }
+                    }
+                }
+            }
+        }
+        else if (searchMethod == SearchType.Search_Vertical && player == null)
+        {
+            RaycastHit hitFront;
+            RaycastHit hitBack;
 
+            if (player == null)
+            {
+                if (moveUpOrRight)
+                {
+                    if (Physics.Raycast(this.transform.position, Vector3.forward, out hitFront, 500))
+                    {
+                        distance = Vector3.Distance(transform.position, hitFront.transform.position);
 
-		if (searchMethod == SearchType.Search_Vertical)
-		{
-			RaycastHit hitFront;
-			RaycastHit hitBack;
+                        if (!ScanForPlayer(hitFront) && player == null)
+                        {
+                            navAgent.SetDestination(hitFront.transform.position);
+                            Debug.Log(distance);
 
-			if (player == null)
-			{
-				if (moveUpOrRight)
-				{
-					if (Physics.Raycast(this.transform.position, Vector3.forward, out hitFront, 500))
-					{
-						distance = Vector3.Distance(transform.position, hitFront.transform.position);
+                            if (distance < 5)
+                            {
+                                moveUpOrRight = false;
+                            }
+                        }
+                    }
+                }
+                else
+                {
+                    if (Physics.Raycast(this.transform.position, -Vector3.forward, out hitBack, 500))
+                    {
+                        distance = Vector3.Distance(transform.position, hitBack.transform.position);
 
-						if (!ScanForPlayer(hitFront) && player == null)
-						{
-							navAgent.SetDestination(hitFront.transform.position);
-							Debug.Log(distance);
+                        if (!ScanForPlayer(hitBack) && player == null)
+                        {
+                            navAgent.SetDestination(hitBack.transform.position);
 
-							if (distance < 5)
-							{
-								moveUpOrRight = false;
-							}
-						}
-					}
-				}
-				else
-				{
-					if (Physics.Raycast(this.transform.position, -Vector3.forward, out hitBack, 500))
-					{
-						distance = Vector3.Distance(transform.position, hitBack.transform.position);
+                            if (distance < 5)
+                            {
+                                moveUpOrRight = true;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        else
+        {
+            navAgent.SetDestination(player.transform.position);
 
-						if (!ScanForPlayer(hitBack) && player == null)
-						{
-							navAgent.SetDestination(hitBack.transform.position);
+            if (Vector3.Distance(transform.position, player.transform.position) < attackDistance)
+            {
+                SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+            }
+        }
 
-							if (distance < 5)
-							{
-								moveUpOrRight = true;
-							}
-						}
-					}
-				}
-			}
-			else
-			{
-				navAgent.SetDestination(player.transform.position);
+        Debug.DrawLine(transform.position, transform.position + Vector3.forward * 10, Color.red);
+        Debug.DrawLine(transform.position, transform.position + -Vector3.forward * 10, Color.red);
+        Debug.DrawLine(transform.position, transform.position + Vector3.right * 10, Color.red);
+        Debug.DrawLine(transform.position, transform.position + -Vector3.right * 10, Color.red);
+    }
 
-				if(Vector3.Distance(transform.position, player.transform.position) < attackDistance)
-				{
-					SceneManager.LoadScene(SceneManager.GetActiveScene().name);
-				}
-			}
-		}
+    //Scans for player for set distance!
+    bool ScanForPlayer(RaycastHit hit)
+    {
+        if (hit.transform.CompareTag("Player"))
+        {
+            if (Vector3.Distance(transform.position, hit.transform.position) < viewDistance)
+            {
+                player = hit.transform.gameObject;
+                navAgent.SetDestination(player.transform.position);
+                player.GetComponent<PlayerController>().playerIsDetected = true;
+                return true;
+            }
+            return false;
+        }
+        return false;
+    }
 
-		Debug.DrawLine(transform.position, transform.position + Vector3.forward * 10, Color.red);
-		Debug.DrawLine(transform.position, transform.position + -Vector3.forward * 10, Color.red);
-		Debug.DrawLine(transform.position, transform.position + Vector3.right * 10, Color.red);
-		Debug.DrawLine(transform.position, transform.position + -Vector3.right * 10, Color.red);
-	}
+    void Alert(Vector3 alertPosition)
+    {
+        navAgent.SetDestination(alertPosition);
+    }
 
-	//Scans for player for set distance!
-	bool ScanForPlayer(RaycastHit hit)
-	{
-		if (hit.transform.CompareTag("Player"))
-		{
-			if (Vector3.Distance(transform.position, hit.transform.position) < viewDistance)
-			{
-				player = hit.transform.gameObject;
-				navAgent.SetDestination(player.transform.position);
-				player.GetComponent<PlayerController>().playerIsDetected = true;
-				return true;
-			}
-			return false;
-		}
-		return false;
-	}
+    void GetOrCreateNavMesh()
+    {
+        navAgent = GetComponent<NavMeshAgent>();
 
-	void Alert(Vector3 alertPosition)
-	{
-		navAgent.SetDestination(alertPosition);
-	}
+        if (navAgent == null)
+        {
+            navAgent = gameObject.AddComponent<NavMeshAgent>();
+        }
+    }
 
-	void GetOrCreateNavMesh()
-	{
-		navAgent = GetComponent<NavMeshAgent>();
+    void GetSOData()
+    {
+        viewDistance = aiLogic.viewDistance;
+        walkSpeed = aiLogic.walkSpeed;
+        runSpeed = aiLogic.runSpeed;
+        canRun = aiLogic.canRun;
+        isLethal = aiLogic.isLethal;
+        attackDistance = aiLogic.attackDistance;
+        searchTime = aiLogic.searchTime;
+        searchDistance = aiLogic.searchDistance;
+    }
 
-		if (navAgent == null)
-		{
-			navAgent = gameObject.AddComponent<NavMeshAgent>();
-		}
-	}
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.transform.CompareTag("Player"))
+        {
+            isChasingPlayer = true;
+            playersLastLocation = collision.transform.position;
+            currentState = AIState.AI_Chasing;
+        }
 
-	void GetSOData()
-	{
-		viewDistance = aiLogic.viewDistance;
-		walkSpeed = aiLogic.walkSpeed;
-		runSpeed = aiLogic.runSpeed;
-		canRun = aiLogic.canRun;
-		isLethal = aiLogic.isLethal;
-		attackDistance = aiLogic.attackDistance;
-		searchTime = aiLogic.searchTime;
-		searchDistance = aiLogic.searchDistance;
-	}
+        if (isChasingPlayer = true)
+        {
+            StartCoroutine(SearchForPlayer());
+        }
+    }
 
-	private void OnCollisionEnter(Collision collision)
-	{
-		if (collision.transform.CompareTag("Player"))
-		{
-			isChasingPlayer = true;
-			playersLastLocation = collision.transform.position;
-			currentState = AIState.AI_Chasing;
-		}
+    IEnumerator SearchForPlayer()
+    {
+        isChasingPlayer = false;
+        currentState = AIState.AI_Searching;
+        yield return new WaitForSeconds(searchTime);
+        currentState = AIState.AI_Patroling;
+    }
 
-		if (isChasingPlayer = true)
-		{
-			StartCoroutine(SearchForPlayer());
-		}
-	}
-
-	IEnumerator SearchForPlayer()
-	{
-		isChasingPlayer = false;
-		currentState = AIState.AI_Searching;
-		yield return new WaitForSeconds(searchTime);
-		currentState = AIState.AI_Patroling;
-	}
-
-	//public float fovAngle = 110f;
-	//public bool playerSighted = false;
+    //public float fovAngle = 110f;
+    //public bool playerSighted = false;
 
 
-	/*private void OnTriggerStay(Collider other)
+    /*private void OnTriggerStay(Collider other)
 	{
 		float angle = Vector3.Angle(transform.position + Vector3.forward, transform.forward);
 
