4177a1ed26981f72c6c3cb85674fc790ad450674
Coby <1011680>
2018-11-14 13:32:09 +1000

    AI now move and detect player. Player detection only works if the AI is looking directly at the player. I'm still working on detection. Check AnotherSample.unity!

--- Assets/Scripts/AI/AIMasterScript.cs
+++ Assets/Scripts/AI/AIMasterScript.cs
@@ -7,7 +7,7 @@ using UnityEngine.AI;
 [RequireComponent(typeof(NavMeshAgent))]
 public class AIMasterScript : MonoBehaviour
 {
-	enum AIState
+	public enum AIState
 	{
 		AI_Patroling,
 		AI_Chasing,
@@ -15,7 +15,7 @@ public class AIMasterScript : MonoBehaviour
 		AI_Alerted
 	};
 
-	enum SearchType
+	public enum SearchType
 	{
 		Search_Snake,
 		Search_Horizontal,
@@ -40,37 +40,53 @@ public class AIMasterScript : MonoBehaviour
 	public Vector3 alertPosition;
 
 	public AI_SO aiLogic;
-	AIState currentState;
-	SearchType searchMethod;
+	public AIState currentState;
+	public SearchType searchMethod;
+
+	public bool moveUpOrRight;
+
+	GameObject player;
+
+	//public float viewDistance = 15;
 
 	void Start()
 	{
 		GetOrCreateNavMesh();
 		GetSOData();
+
+		moveUpOrRight = true;
 	}
 
 	void Update()
 	{
+
 		switch (currentState)
 		{
 			case AIState.AI_Patroling:
-				Patrol();
-			break;
+				Search();
+
+				//Patrol();
+				break;
 
 			case AIState.AI_Chasing:
-				Chase();
-			break;
+				//Chase();
+				Search();
+
+				break;
 
 			case AIState.AI_Searching:
 				Search();
-			break;
+
+				break;
 
 			case AIState.AI_Alerted:
-				Alert(alertPosition);
-			break;
+				Search();
+
+				//Alert(alertPosition);
+				break;
 
 			default:
-			break;
+				break;
 		}
 	}
 
@@ -78,7 +94,7 @@ public class AIMasterScript : MonoBehaviour
 	{
 		if (Vector3.Distance(transform.position, patrolPositions[nextPatrolPosition].transform.position) < minDistanceFromCheckpoint) //Todo - Replace with SO Editable
 		{
-			if(patrolPositions.Length -1 < nextPatrolPosition + 1)
+			if (patrolPositions.Length - 1 < nextPatrolPosition + 1)
 			{
 				nextPatrolPosition = 0;
 			}
@@ -98,9 +114,118 @@ public class AIMasterScript : MonoBehaviour
 
 	void Search()
 	{
-		Vector3 randomNavPosition = new Vector3(Random.Range(-50,50), Random.Range(-50,50));
+		Vector3 randomNavPosition = new Vector3(Random.Range(-50, 50), Random.Range(-50, 50));
 		NavMeshHit hitPoint;
 		NavMesh.SamplePosition(randomNavPosition, out hitPoint, 50, 1);
+
+		//Scan Vector 3.Up * dist + -Vector3.forward * dist
+
+		float distance = 0.0f;
+
+		if (searchMethod == SearchType.Search_Horizontal)
+		{
+			RaycastHit hitRight;
+			RaycastHit hitLeft;
+
+			if (moveUpOrRight)
+			{
+				if (Physics.Raycast(this.transform.position, Vector3.right, out hitRight, 500))
+				{
+					distance = Vector3.Distance(transform.position, hitRight.transform.position);
+					if (!ScanForPlayer(hitRight) && player == null)
+					{
+						navAgent.SetDestination(hitRight.transform.position);
+						Debug.Log(distance);
+
+						if (distance < 5)
+						{
+							moveUpOrRight = false;
+						}
+					}
+				}
+			}
+			else
+			{
+				if (Physics.Raycast(this.transform.position, -Vector3.right, out hitLeft, 500))
+				{
+					if (!ScanForPlayer(hitLeft) && player == null)
+					{
+						distance = Vector3.Distance(transform.position, hitLeft.transform.position);
+
+						navAgent.SetDestination(hitLeft.transform.position);
+
+						if (distance < 5)
+						{
+							moveUpOrRight = true;
+						}
+					}
+				}
+			}
+		}
+
+
+		if (searchMethod == SearchType.Search_Vertical)
+		{
+			RaycastHit hitFront;
+			RaycastHit hitBack;
+
+			if (moveUpOrRight)
+			{
+				if (Physics.Raycast(this.transform.position, Vector3.forward, out hitFront, 500))
+				{
+					distance = Vector3.Distance(transform.position, hitFront.transform.position);
+
+					if (!ScanForPlayer(hitFront) && player == null)
+					{
+						navAgent.SetDestination(hitFront.transform.position);
+						Debug.Log(distance);
+
+						if (distance < 5)
+						{
+							moveUpOrRight = false;
+						}
+					}
+				}
+			}
+			else
+			{
+				if (Physics.Raycast(this.transform.position, -Vector3.forward, out hitBack, 500))
+				{
+					distance = Vector3.Distance(transform.position, hitBack.transform.position);
+
+					if (!ScanForPlayer(hitBack) && player == null)
+					{
+						navAgent.SetDestination(hitBack.transform.position);
+
+						if (distance < 5)
+						{
+							moveUpOrRight = true;
+						}
+					}
+				}
+			}
+		}
+
+		Debug.DrawLine(transform.position, transform.position + Vector3.forward * 10, Color.red);
+		Debug.DrawLine(transform.position, transform.position + -Vector3.forward * 10, Color.red);
+		Debug.DrawLine(transform.position, transform.position + Vector3.right * 10, Color.red);
+		Debug.DrawLine(transform.position, transform.position + -Vector3.right * 10, Color.red);
+	}
+
+	//Scans for player for set distance!
+	bool ScanForPlayer(RaycastHit hit)
+	{
+		if (hit.transform.CompareTag("Player"))
+		{
+			if (Vector3.Distance(transform.position, hit.transform.position) < viewDistance)
+			{
+				player = hit.transform.gameObject;
+				navAgent.SetDestination(player.transform.position);
+				return true;
+			}
+			return false;
+		}
+		return false;
 	}
 
 	void Alert(Vector3 alertPosition)
@@ -112,7 +237,7 @@ public class AIMasterScript : MonoBehaviour
 	{
 		navAgent = GetComponent<NavMeshAgent>();
 
-		if(navAgent == null)
+		if (navAgent == null)
 		{
 			navAgent = gameObject.AddComponent<NavMeshAgent>();
 		}
@@ -132,14 +257,14 @@ public class AIMasterScript : MonoBehaviour
 
 	private void OnCollisionEnter(Collision collision)
 	{
-		if(collision.transform.CompareTag("Player"))
+		if (collision.transform.CompareTag("Player"))
 		{
 			isChasingPlayer = true;
 			playersLastLocation = collision.transform.position;
 			currentState = AIState.AI_Chasing;
 		}
-		
-		if(isChasingPlayer = true)
+
+		if (isChasingPlayer = true)
 		{
 			StartCoroutine(SearchForPlayer());
 		}
@@ -152,4 +277,29 @@ public class AIMasterScript : MonoBehaviour
 		yield return new WaitForSeconds(searchTime);
 		currentState = AIState.AI_Patroling;
 	}
+
+	//public float fovAngle = 110f;
+	//public bool playerSighted = false;
+
+
+	/*private void OnTriggerStay(Collider other)
+	{
+		float angle = Vector3.Angle(transform.position + Vector3.forward, transform.forward);
+
+		if (angle < fovAngle * 0.5f)
+		{
+			RaycastHit hit;
+
+			if (Physics.Raycast(transform.position + transform.up, Vector3.forward, out hit, fovDistance))
+			{
+				if (hit.collider.gameObject.CompareTag("Player"))
+				{
+					playerSighted = true;
+					Debug.Log("I SEE YOU!");
+				}
+			}
+		}
+
+
+	}*/
 }
