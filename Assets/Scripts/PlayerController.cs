b131bc9cdaf4327756a49640767b49dcc31e3785
Jiyoung Kim <1010215@student.sae.edu.au>
2018-11-18 21:49:00 +1000

    movement restricted to grid virus acts as a mobile wall

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,20 +6,19 @@ using UnityEngine.UI;
 [RequireComponent(typeof(Rigidbody))]
 public class PlayerController : MonoBehaviour
 {
-
+    public bool isWalking = false;
+    public bool isRunning = false;
 	public float walkSpeed;
 	public float runSpeed;
 	Vector2 inputDirection;
 	Vector3 startPosition;
 
 	public bool playerIsDetected;
-	Animator anim;
 
 	Rigidbody playerRB;
 
 	void Start()
 	{
-		anim = gameObject.GetComponent<Animator>();
 		playerRB = GetComponent<Rigidbody>();
 
         transform.position += new Vector3(0,0.1f,0);
@@ -27,9 +26,9 @@ public class PlayerController : MonoBehaviour
 		walkSpeed = 500;
 		runSpeed = 900;
 
-        playerRB.constraints = RigidbodyConstraints.None;
+        playerRB.constraints = RigidbodyConstraints.FreezePositionY;
         playerRB.constraints = RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationZ;
-        playerRB.useGravity = true;
+        playerRB.useGravity = false;
     }
 
     public float speed;
@@ -55,30 +54,26 @@ public class PlayerController : MonoBehaviour
 			playerRB.velocity = new Vector3(playerRB.velocity.x, 0, 0);
 		}
 
-		if (movement.magnitude != 0)
-		{
-			if (playerIsDetected)
-			{
-				anim.SetBool("isRunning", true);
-			}
-			else
-			{
-				anim.SetBool("isWalking", true);
-			}
-		}
-		else
-		{
-			if (playerIsDetected)
-			{
-				anim.SetBool("isRunning", false);
-			}
-			else
-			{
-				anim.SetBool("isWalking", false);
-			}
-		}
+        if (movement.magnitude != 0)
+        {
+            if (playerIsDetected)
+            {
+                isRunning = true;
+            }
+            else
+            {
+                isWalking = true;
+            }
+        }
+        else
+        {
+            isRunning = false;
+            isWalking = false;
+        }
 
-		if (playerIsDetected)
+
+
+        if (playerIsDetected)
 		{
 			playerRB.AddForce(movement * runSpeed * Time.deltaTime * 15);
 
---
+++ Assets/Scripts/VirusMovement.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VirusMovement : MonoBehaviour {
+
+    float speed;
+    string direction;
+
+	// Use this for initialization
+	void Start () {
+        speed = 80f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Rotate(Vector3.up * speed * Time.deltaTime);
+    }
+}
