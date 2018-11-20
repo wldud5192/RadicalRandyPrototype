3eceb5f8920aac0fdcf07e64f9b61149ca3eb2bb
Jiyoung Kim <1010215@student.sae.edu.au>
2018-11-20 19:03:01 +1000

    Test: Player dashes instead of walking... double dash isn't not working yet

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,57 +6,72 @@ using UnityEngine.UI;
 [RequireComponent(typeof(Rigidbody))]
 public class PlayerController : MonoBehaviour
 {
+    public GameObject dashEffect;
+	float dashSpeed;
+    int dashCheck;
 
-	public float walkSpeed;
-	public float runSpeed;
+    float dashTime;
+    float startDashTime;
+    int direction;
+    
 	Vector2 inputDirection;
 	Vector3 startPosition;
 
 	public bool playerIsDetected;
 	Animator anim;
-
 	Rigidbody playerRB;
 
-	void Start()
+    float doorDropPos;
+    float doorSpeed;
+    GameObject[] Unlocked;
+
+    void Start()
 	{
-		anim = gameObject.GetComponent<Animator>();
+        startDashTime = 0.2f;
+        dashTime = startDashTime;
+		anim = GetComponent<Animator>();
 		playerRB = GetComponent<Rigidbody>();
 
         transform.position += new Vector3(0,0.1f,0);
 
-		walkSpeed = 2000;
-		runSpeed = 4000;
+		dashSpeed = 11.5f;
 
         playerRB.constraints = RigidbodyConstraints.None;
 		playerRB.constraints = RigidbodyConstraints.FreezeRotation;
         playerRB.useGravity = true;
     }
+    
 
-    public float speed;
-	public float doorDropPos;
-	public float doorSpeed;
-	GameObject[] Unlocked;
+    void Update()
+    {
+        UpdateCharacterToDirection();
 
-	void Update()
-	{
-		float moveHorizontal = Input.GetAxisRaw("Horizontal");
-		float moveVertical = Input.GetAxisRaw("Vertical");
+        if (direction == 0)
+        {
+            SetDirection();
+        } else
+        {
+            if(dashTime <= 0)
+            {
+                direction = 0;
+                dashTime = startDashTime;
+                playerRB.velocity = Vector2.zero;
 
-		Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
-		movement = movement.normalized;
-
-		HandleInputRotation(moveHorizontal, moveVertical);
-
-		if(moveHorizontal == 0)
-		{
-			playerRB.velocity = new Vector3(0, 0, playerRB.velocity.z);
-		}
-		if(moveVertical == 0)
-		{
-			playerRB.velocity = new Vector3(playerRB.velocity.x, 0, 0);
-		}
-
-		if (movement.magnitude != 0)
+            }
+            else if (dashTime > 0)
+            {
+                dashTime -= Time.deltaTime;
+                if (dashCheck == 0)
+                {
+                    Dash();
+                } else
+                {
+                    DoubleDash();
+                }
+            }
+        }
+        
+		if (playerRB.velocity.magnitude > 1)
 		{
 			if (playerIsDetected)
 			{
@@ -81,38 +96,94 @@ public class PlayerController : MonoBehaviour
 
 		if (playerIsDetected)
 		{
-			playerRB.AddForce(movement * runSpeed * Time.deltaTime * 15);
-
-            playerRB.velocity = Vector3.ClampMagnitude(playerRB.velocity, 3);
         }
 		else
 		{
-			playerRB.AddForce(movement * walkSpeed * Time.deltaTime * 15);
-
-            playerRB.velocity = Vector3.ClampMagnitude(playerRB.velocity, 1);
         }
 
 	}
+    
 
-	void HandleInputRotation(float moveHorizontal, float moveVertical)
-	{
-		if (moveHorizontal > 0)
-		{
-			transform.rotation = Quaternion.Euler(0, 90,0);
-		}
-		else if (moveHorizontal < 0)
-		{
-			transform.rotation = Quaternion.Euler(0, 270,0);
-		}
-		else if(moveVertical > 0)
-		{
-			transform.rotation = Quaternion.Euler(0,0,0);
-		}
-		else if(moveVertical < 0)
-		{
-			transform.rotation = Quaternion.Euler(0,180,0);
-		}
-	}
+    IEnumerator doubleDashCooler()
+    {
+        dashCheck++;
+        yield return new WaitForSeconds(0.3f);
+        dashCheck = 0;
+    }
+
+    void UpdateCharacterToDirection()
+    {
+        float moveHorizontal = Input.GetAxisRaw("Horizontal");
+        float moveVertical = Input.GetAxisRaw("Vertical");
+        Vector3 movement = new Vector3(moveHorizontal, 0f, moveVertical);
+        if (movement.magnitude != 0)
+        {
+            transform.rotation = Quaternion.LookRotation(movement);
+        }
+    }
+
+    void SetDirection()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            DashEffect();
+            direction = 1;
+        }
+        else if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            DashEffect();
+            direction = 2;
+        }
+        else if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            DashEffect();
+            direction = 3;
+        }
+        else if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            DashEffect();
+            direction = 4;
+        }
+
+    }
+
+    void DashEffect()
+    {
+        Instantiate(dashEffect, transform.position, Quaternion.identity);
+        GameObject clone = Instantiate(dashEffect, transform.position, Quaternion.identity);
+        Destroy(clone, 1.5f);
+    }
+
+    void Dash()
+    {
+        StartCoroutine(doubleDashCooler());
+        
+        if (direction == 1)
+        {
+            playerRB.velocity = Vector3.left * dashSpeed;
+        }
+        else if (direction == 2)
+        {
+            playerRB.velocity = Vector3.right * dashSpeed;
+        }
+        else if (direction == 3)
+        {
+            playerRB.velocity = Vector3.forward * dashSpeed;
+        }
+        else if (direction == 4)
+        {
+            playerRB.velocity = Vector3.back * dashSpeed;
+        }
+
+    }
+    
+
+    void DoubleDash()
+    {
+        Debug.Log("DD");
+
+       
+    }
 
 	// Unlocking Doors
 	private void OnTriggerStay(Collider other)
