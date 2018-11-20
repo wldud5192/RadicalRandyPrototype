825cc5778316f4efe4d0e4f0fa6de3c391ea5b50
Jiyoung Kim <1010215@student.sae.edu.au>
2018-11-20 11:17:25 +1000

    Level Iteration + Main Menu + Level UI

---
+++ Assets/LevelUI.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class LevelUI : MonoBehaviour {
+    TMP_Text m_TextComponent;
+
+    void Awake ()
+    {
+        m_TextComponent = GetComponent<TMP_Text>();
+        Scene scene = SceneManager.GetActiveScene();
+        m_TextComponent.text = "Level: " + scene.buildIndex;
+    }
+
+}
